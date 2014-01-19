1caea76535eeb8bbd1225d47b22920784b6a2d80
Nathan Walker <nathan.walker@bizbuilt.com>
2014-01-18 20:33:54 -0800

    Rebuilding with latest fix and creating new tag

--- angular-deckgrid.js
+++ angular-deckgrid.js
@@ -1,4 +1,4 @@
-/*! angular-deckgrid (v0.2.1) - Copyright: 2013, André König (andre.koenig@posteo.de) - MIT */
+/*! angular-deckgrid (v0.2.2) - Copyright: 2013, André König (andre.koenig@posteo.de) - MIT */
 /*
  * angular-deckgrid
  *
