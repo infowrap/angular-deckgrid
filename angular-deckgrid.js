90abc81f941776ac95bbdd8bdafdd56f2038f523
Nathan Walker <nathan.walker@bizbuilt.com>
2014-02-05 11:37:02 -0800

    fixing dependency

--- angular-deckgrid.js
+++ angular-deckgrid.js
@@ -1,4 +1,4 @@
-/*! angular-deckgrid (v0.4.9) - Copyright: 2013, André König (andre.koenig@posteo.de) - MIT */
+/*! angular-deckgrid (v0.5.0) - Copyright: 2013, André König (andre.koenig@posteo.de) - MIT */
 /*
  * angular-deckgrid
  *
